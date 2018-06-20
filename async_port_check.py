9fc692fa2cb9d6d9f7233d635b44bbd2555bda54
Günhan Oral <gunhan.oral@gmail.com>
2018-06-20 08:37:38 +0300

    removed unnecessary asyncio gather

--- async_port_check.py
+++ async_port_check.py
@@ -1,19 +1,41 @@
-import asyncio
 import ipaddress
+import asyncio
+import time
+
+now = time.time()
+
 async def check_port(ip, port, loop):
-	try:
-		reader, writer = await asyncio.wait_for(asyncio.open_connection(ip, port, loop=loop), timeout=3)
-		writer.close()
-		return (ip, port, True)
-	except:
-		return (ip, port, False)
+        conn = asyncio.open_connection(ip, port, loop=loop)
+        try:
+                reader, writer = await asyncio.wait_for(conn, timeout=3)
+                return (ip, port, True)
+        except:
+                return (ip, port, False)
+
+async def check_port_sem(sem, ip, port, loop):
+        async with sem:
+                return await check_port(ip, port, loop)
+
 async def run(dests, ports, loop):
-	tasks = [asyncio.ensure_future(check_port(d, p, loop)) for d in dests for p in ports]
-	responses = await asyncio.gather(*tasks)
-	return responses
-dests = ['10.1.1.1', '192.168.1.1', '172.27.3.5']
-ports = [21, 22, 23, 80, 443, 3389]
+        sem = asyncio.Semaphore(400) #Change this value for limitation
+        tasks = [asyncio.ensure_future(check_port_sem(sem, d, p, loop)) for d in dests for p in ports]
+        responses = await asyncio.gather(*tasks)
+        return responses
+
+networks = ['1.1.1.0/24', '2.2.2.0/24']
+hosts = ['3.3.3.3', '4.4.4.4']
+
+dests = []
+for nw in networks:
+    for ip in ipaddress.IPv4Network(nw).hosts():
+        dests.append(str(ip))
+
+dests.extend(hosts)
+ports = [22,23,80,443]
+
 loop = asyncio.get_event_loop()
-future = asyncio.ensure_future(run(dests, ports, loop))
-loop.run_until_complete(future)
-print('Results: ', future.result())
+future = loop.run_until_complete(run(dests, ports, loop))
+print('Done. Total time: ', time.time() - now)
+print('\n')
+for ip, port, res in future:
+    print('{}\t{}\t{}'.format(ip,port,res))
